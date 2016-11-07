8a0887024932bf912cf2155b25bb662c62d6fc45
candyhh <17764576286@163.com>
2016-11-07 14:45:35 +0800

    样式

---
+++ public/javascripts/main.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function(){
+  //页面跳转
+  $(".next-btn").on('click',function(){
+    window.location.href="next";
+  });
+  $('.return-index-btn').on('click',function () {
+    window.location.href="/";
+  })
+  $('.subway-btn').on('click',function () {
+    window.location.href="subway";
+  })
+  // 达人介绍
+  $('.daren-wang-btn').on('click',function () {
+    var indexHeight = $('.container').height();
+    $('.mengban').height(indexHeight);
+    $('.mengban').show();
+    $('.intro-wang').show();
+  })
+  $('.daren-tang-btn').on('click',function () {
+    var indexHeight = $('.container').height();
+    $('.mengban').height(indexHeight);
+    $('.mengban').show();
+    $('.intro-pang').show();
+  })
+  $('.daren-pang-btn').on('click',function () {
+    var indexHeight = $('.container').height();
+    $('.mengban').height(indexHeight);
+    $('.mengban').show();
+    $('.intro-tang').show();
+  })
+  $('.daren-ku-btn').on('click',function () {
+    var indexHeight = $('.container').height();
+    $('.mengban').height(indexHeight);
+    $('.mengban').show();
+    $('.intro-ku').show();
+  })
+  $('.mengban').on('click',function () {
+    $('.daren-intro').hide();
+    $('.mengban').hide();
+  })
+  // 报名
+  $('.sign-btn').on('click',function () {
+    window.location.href="sign"
+  })
+});
--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,66 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.get('/next', function(req, res, next) {
+  var isApp = false;
+  if (req.query.DEVICE== 'ios' || req.query.DEVICE == 'android') {
+    isApp = true;
+  }
+  var thisUrl = req.url;
+  var shareUrl = encodeURIComponent((global.browserURL + thisUrl).split('#')[0]);
+  console.log('shareUrl.................'+(global.browserURL + thisUrl).split('#')[0]);
+  superagent
+    .get(global.wechatURL + '/wechat_api/jsconfig?url=' + shareUrl)
+    .end(function(err2, res2) {
+      if (res2 !== undefined && res2.ok) {
+        res2.body.browserUrl = global.browserURL;
+        res2.body.flag = isApp;
+        res.render('next',res2.body);
+      } else {
+        console.error('微信分享api错误。');
+      }
+    });
+});
+
+router.get('/subway', function(req, res, next) {
+  var isApp = false;
+  if (req.query.DEVICE== 'ios' || req.query.DEVICE == 'android') {
+    isApp = true;
+  }
+  var thisUrl = req.url;
+  var shareUrl = encodeURIComponent((global.browserURL + thisUrl).split('#')[0]);
+  console.log('shareUrl.................'+(global.browserURL + thisUrl).split('#')[0]);
+  superagent
+    .get(global.wechatURL + '/wechat_api/jsconfig?url=' + shareUrl)
+    .end(function(err2, res2) {
+      if (res2 !== undefined && res2.ok) {
+        res2.body.browserUrl = global.browserURL;
+        res2.body.flag = isApp;
+        res.render('subway',res2.body);
+      } else {
+        console.error('微信分享api错误。');
+      }
+    });
+});
+
+router.get('/sign', function(req, res, next) {
+  var isApp = false;
+  if (req.query.DEVICE== 'ios' || req.query.DEVICE == 'android') {
+    isApp = true;
+  }
+  var thisUrl = req.url;
+  var shareUrl = encodeURIComponent((global.browserURL + thisUrl).split('#')[0]);
+  console.log('shareUrl.................'+(global.browserURL + thisUrl).split('#')[0]);
+  superagent
+    .get(global.wechatURL + '/wechat_api/jsconfig?url=' + shareUrl)
+    .end(function(err2, res2) {
+      if (res2 !== undefined && res2.ok) {
+        res2.body.browserUrl = global.browserURL;
+        res2.body.flag = isApp;
+        res.render('sign',res2.body);
+      } else {
+        console.error('微信分享api错误。');
+      }
+    });
+});
 module.exports = router;
