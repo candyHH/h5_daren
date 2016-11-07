3340046fe7e613b8707f3781b81a57dd3c584abb
candyhh <17764576286@163.com>
2016-11-07 10:03:13 +0800

    superagent lose

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var superagent = require('superagent');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var isApp = false;
+  if (req.query.DEVICE== 'ios' || req.query.DEVICE == 'android') {
+    isApp = true;
+  }
+  var thisUrl = req.url;
+  var shareUrl = encodeURIComponent((global.browserURL + thisUrl).split('#')[0]);
+  console.log('shareUrl.................'+(global.browserURL + thisUrl).split('#')[0]);
+  superagent
+    .get(global.wechatURL + '/wechat_api/jsconfig?url=' + shareUrl)
+    .end(function(err2, res2) {
+      if (res2 !== undefined && res2.ok) {
+        res2.body.browserUrl = global.browserURL;
+        res2.body.flag = isApp;
+        res.render('index',res2.body);
+      } else {
+        console.error('微信分享api错误。');
+      }
+    });
 });
 
 module.exports = router;
